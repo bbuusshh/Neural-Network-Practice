d858270ad8d1c79086773a9aa1c99b7437c09cc6
Evangelos Vandoros <vandvag1994@gmail.com>
2019-05-27 22:02:13 +0200

    Added comments to functions

--- Code/neuralnetwork.py
+++ Code/neuralnetwork.py
@@ -2,13 +2,28 @@ import numpy as np
 
 
 class NeuralNetwork:
+    '''
+    Creates a Neural Network with a structure defined by
+    layer_sizes
+    '''
 
     def __init__(self, layer_sizes):
+        '''
+        Initializes the Neural Network weights and biases with random values
+        weight_shapes: The shapes for every weight matrix
+        self.weights: Initialiazation of the weight matrices for each layer based on a standard_normal distribution
+        self.biases: Initialization of the biases for each layer with ones
+        self.layer_sizes: Tuple containing the size of each layer
+        '''
         weight_shapes = [(a, b) for a, b in zip(layer_sizes[1:], layer_sizes[:-1])]
         self.weights = [np.random.standard_normal(s) / s[1]**.5 for s in weight_shapes]
         self.biases = [np.ones((s, 1)) for s in layer_sizes[1:]]
+        self.layer_sizes = layer_sizes
 
     def forward(self, a):
+        '''
+        Calculates the forward progpa
+        '''
         for w, b in zip(self.weights, self.biases):
             a = self.activation(np.matmul(w, a) + b)
         return a
--- Code/program.py
+++ Code/program.py
@@ -4,8 +4,21 @@ import numpy as np
 # Importing the data
 
 
-def import_data(filename):
-    data = np.loadtxt(filename)
+def import_data(path):
+    '''
+    Function for importing the data from the data file
+    ----------------------------------------------------
+    Inputs
+    ----------------------------------------------------
+    path: Path to the data file (absolute or relative)
+    ----------------------------------------------------
+    Outputs
+    ----------------------------------------------------
+    input_data: The data to be inputed to the Neural network
+    output_data: The labels of the input data
+    '''
+
+    data = np.loadtxt(path)
     input_data = data[:, :2]
     output_data = data[:, -1]
 
@@ -13,6 +26,18 @@ def import_data(filename):
 
 
 def get_predictions(data_in, network):
+    '''
+    Get a prediction vector using forward propagation
+    --------------------------------------------------
+    Inputs:
+    --------------------------------------------------
+    data_in: Data to be used for forward propagation
+    network: Neural Network to be used
+    --------------------------------------------------
+    Output:
+    --------------------------------------------------
+    np.array(predictions): vector of outputs of the NN
+    '''
     predictions = []
 
     for data in data_in:
@@ -34,8 +59,7 @@ if __name__ == '__main__':
     data_in, data_out = import_data(filename)
 
     # Initialize the Neural Network
-    layer_sizes = (2, 1)
+    layer_sizes = (2, 20, 1)
     net = nn.NeuralNetwork(layer_sizes)
     # Get predictions from forward propagation
     predictions = get_predictions(data_in, net)
-    print(predictions.shape)
